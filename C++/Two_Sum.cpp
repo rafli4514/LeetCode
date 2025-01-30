c1cc2590a1da5c915a392253b002d8ff66bc816b
Muhammad Rafli Aulia <112487123+rafli4514@users.noreply.github.com>
2025-01-30 16:46:17 +0700

    Create Two_Sum.cpp

---
+++ C++/Two_Sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int res[2];
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = i + 1; j < nums.size(); j++){
+                if(nums[i] + nums[j] == target){
+                     res[0] = i;
+                     res[1] = j;
+                     return {res[0], res[1]};
+                }
+            }
+        }
+        return {};
+    }
+};
+
+// O(n^2)
